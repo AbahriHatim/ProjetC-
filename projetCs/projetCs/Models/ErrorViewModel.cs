b6581bc7c683a078dfe8f2bad65cec26bcc99f4f
Hatim Abahri <hatimabahri9@gmail.com>
2023-12-21 17:23:52 +0100

    first push

---
+++ projetCs/projetCs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetCs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
